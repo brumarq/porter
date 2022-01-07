abcbd052e05de11aa49549c163ef6f313a6700cf
Bruno Marques <brunocm@pm.me>
2022-01-07 18:49:51 +0100

    db connection for horoku

--- app/config.php
+++ app/config.php
@@ -1,8 +1,21 @@
 <?php
-    $servername = "mysql";
-    $dbusername = "developer";
-    $dbpassword = "secret123";
-    $databasename = "porter";
+    if (getenv('DATABASE_URL') != "") {
+        $herokuDb = parse_url(getenv('DATABASE_URL'));
+
+        $type = 'pgsql';
+        $servername = $herokuDb['host'];
+        $dbusername = $herokuDb['user'];
+        $dbpassword = $herokuDb['pass'];
+        $databasename = ltrim($herokuDb['path'], '/');
+        
+    }else {
+        
+        $type = "mysql";
+        $servername = "mysql";
+        $dbusername = "developer";
+        $dbpassword = "secret123";
+        $databasename = "porter";
+    }
     
-    $conn = new PDO("mysql:host=$servername;dbname=$databasename", $dbusername, $dbpassword);
+    $conn = new PDO("$type:host=$servername;dbname=$databasename", $dbusername, $dbpassword);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
